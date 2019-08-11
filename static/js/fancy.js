4bb5adb65dcecade58f062a9b2c0f0250940bf14
mjarenyap <mjaren.yap@gmail.com>
2019-08-11 16:24:48 +0800

    Added sponsors to page

--- static/js/fancy.js
+++ static/js/fancy.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
     var timelines = $('.timeline');
     $(timelines[timelines.length - 1]).css('border-color', 'transparent');
+    $($('.hidden')[0]).removeClass('hidden');
+});
+
+$(document).scroll(function() {
+	var hidden_sections = $('.hidden');
+	for(let x = 0; x < hidden_sections.length; x++)
+		$(this).scrollTop() >= $(hidden_sections[x]).offset().top - 300 ? $(hidden_sections[x]).removeClass('hidden') : x;
 });
