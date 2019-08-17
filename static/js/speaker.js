d2a1ad8b4857c27445cfe2ff76325234ab7a8655
mjarenyap <mjaren.yap@gmail.com>
2019-08-17 23:33:04 +0800

    Reduced size of banners. Workshop modals

--- static/js/speaker.js
+++ static/js/speaker.js
@@ -1,10 +1,14 @@
 // speaker JS functionality
 
-// { name : [bio, photo] }
-var profiles = {};
+var profiles = {}; // { name : [bio, photo] }
+var workshops = {}; // { topic : [facilitator, description] }
 
 function addProfile(name, title, topic, bio, photo, linkedin) {
 	profiles[name] = [title, topic, bio, photo, linkedin];
+}
+
+function addWorkshop(topic, facilitator, description) {
+	workshops[topic] = [facilitator, description];
 }
 
 function updateModalSpeaker(name) {
@@ -23,6 +27,20 @@ function updateModalSpeaker(name) {
 			<!--<a href="#" target="blank"><i class="fab fa-twitter"></i></a>-->
 			<a href="${profiles[name][4]}" target="blank"><i class="fab fa-linkedin"></i></a>
 		</div>
+		`
+		);
+
+	$('#modal').show();
+	$('body').addClass('stiff');
+}
+
+function updateModalWorkshop(topic) {
+	$('#modal-wrapper .content-wrapper').append(
+		`<h2 class="yellow">${topic}</h2>
+		<h3 class="white title-topic">${workshops[topic][0]}</h3>
+		<br/>
+		<p>${workshops[topic][1]}</p>
+		<br/>
 		`
 		);
 
