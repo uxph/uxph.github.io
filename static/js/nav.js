0b5ecfdde0f8fca89add5ca528ad29c42d538382
mjarenyap <mjaren.yap@gmail.com>
2020-03-25 17:54:27 +0800

    Refinements to accomplished revisions from feedback

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,9 +1,31 @@
 $(document).ready(function() {
+	
+	if(window.location.pathname != '/') {
+		$('#nav-logo').addClass('show');
+		$("#main-nav div.wrapper").addClass('justify-content-between');
+		$("#main-nav div.wrapper").removeClass('justify-content-center');
+	}
+
 	$(window).scroll(function() {
 		if($(window).scrollTop() > 0)
 			$('#main-nav').addClass('shadow-sm');
 
 		else $('#main-nav').removeClass('shadow-sm');
+
+		// show uxph logo
+		if(window.location.pathname == '/') {
+			if($(window).scrollTop() > 200) {
+				$('#nav-logo').addClass('show');
+				$("#main-nav div.wrapper").addClass('justify-content-between');
+				$("#main-nav div.wrapper").removeClass('justify-content-center');
+			}
+
+			else {
+				$('#nav-logo').removeClass('show');
+				$("#main-nav div.wrapper").removeClass('justify-content-between');
+				$("#main-nav div.wrapper").addClass('justify-content-center');
+			}
+		}
 	});
 
 	$("#menu-bar").click(function() {
@@ -19,5 +41,4 @@ function setActive(n) {
 	let main_nav = $('#main-nav ul li');
 	$(main_nav).removeClass('active');
 	$(main_nav[n]).addClass('active');
-	console.log(main_nav, n);
 }
