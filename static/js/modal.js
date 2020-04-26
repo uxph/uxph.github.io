dfe6fa9cf5be7a646f98e31501c40c51c0730a51
mjarenyap <mjaren.yap@gmail.com>
2020-04-26 16:25:27 +0800

    Event ended banner; modal implmentation

---
+++ static/js/modal.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('#modal-close').click(function() {
+        $('#modal-overlay').hide();
+        $('body').removeClass('stiff');
+    });
+
+    $('.user-card').click(function() {
+        $('body').addClass('stiff');
+        $('#modal-overlay').show();
+        $('#modal-container').empty();
+        $(this).clone().appendTo('#modal-container');
+
+        $('#modal-container *').removeClass('d-none');
+        $('#modal-container .user-card').removeClass('shadow-sm');
+        $('#modal-container .user-card').removeClass('user-card');
+        $('#modal-container p').addClass('text-center');
+
+        let socials = $.parseHTML($('#modal-container .socials-wrapper').clone().text());
+        console.log("socials", socials);
+
+        $('#modal-container .socials-wrapper').empty();
+        $('#modal-container .socials-wrapper').html(socials);
+        $('#modal-container .socials-wrapper').addClass('d-flex justify-content-center margin-y-24');
+        $('#modal-container .socials-wrapper i').addClass('fa-2x margin-x-16');
+    });
+});
